dab85eef0b86299166b42d8198807dec4fd47235
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-12-30 12:49:58 -0300

    bomb

--- src/config/rateLimiter.ts
+++ src/config/rateLimiter.ts
@@ -1,7 +1,8 @@
-import type {Options} from "elysia-rate-limit";
+import type { Options } from "elysia-rate-limit";
 
 export const rateLimiterOptions: Partial<Options> = {
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	errorResponse: "Too many requests from this IP, please try again after 15 minutes",
-	headers: true,
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    errorResponse: "Too many requests from this IP, please try again after 15 minutes",
+    headers: true,
+    duration: 5 * 60 * 1000, // 5 minutes
 };
