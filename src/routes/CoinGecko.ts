83d3b4333f98653960be71eeaf54d47464bd9c99
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2025-02-01 16:19:34 -0300

    bomb

--- src/routes/CoinGecko.ts
+++ src/routes/CoinGecko.ts
@@ -1,9 +1,10 @@
-import {Elysia, t} from "elysia";
+import { Elysia, t } from "elysia";
 import {
-	detailCryptoMarketChart,
-	detailCryptoDescriptionData,
-	listCryptoCurrencies,
-	searchCrypto,
+    detailCryptoMarketChart,
+    detailCryptoDescriptionData,
+    listCryptoCurrencies,
+    searchCrypto,
+    getTrendingCryptos,
 } from "@/proxy/CoinGecko";
 import TErrorFormatter from "@/traits/TErrorFormatter";
 import queryParamsEncode from "@/traits/queryParamsEncode";
@@ -15,201 +16,209 @@ const to = new Date();
 from.setDate(from.getDate() - 31);
 
 export function CoinGeckoRoutes(app: Elysia): Elysia {
-	return app.use(
-		new Elysia({
-			name: "Coin Gecko Routes",
-			aot: false,
-			prefix: "/coins",
-			detail: {
-				tags: ["CoinGecko"],
-			},
-		})
-			.get(
-				"/markets",
-				async ({query}) =>
-					listCryptoCurrencies({
-						query: queryParamsEncode(query),
-					}),
-				{
-					query: t.Object({
-						vs_currency: t.Optional(
-							t.String({
-								examples: ["usd", "btc", "eth"],
-								default: "usd",
-								minLength: 3,
-								error({errors}) {
-									TErrorFormatter(errors);
-								},
-							}),
-						),
-						per_page: t.Optional(
-							t.Number({
-								examples: [12, 20, 50, 100],
-								default: 12,
-								minimum: 1,
-								error({errors}) {
-									TErrorFormatter(errors);
-								},
-							}),
-						),
-						page: t.Optional(
-							t.Number({
-								examples: [1, 2, 5, 10],
-								default: 1,
-								minimum: 1,
-								error({errors}) {
-									TErrorFormatter(errors);
-								},
-							}),
-						),
-						precision: t.Optional(
-							t.Number({
-								examples: [0, 1, 2],
-								default: 2,
-								minimum: 1,
-								error({errors}) {
-									TErrorFormatter(errors);
-								},
-							}),
-						),
-						price_change_percentage: t.Optional(
-							t.String({
-								examples: ["1h", "24h", "7d", "14d", "30d", "200d", "1y"],
-								error({errors}) {
-									TErrorFormatter(errors);
-								},
-							}),
-						),
-						order: t.Optional(
-							t.String({
-								examples: [
-									"market_cap_desc",
-									"market_cap_asc",
-									"volume_asc",
-									"volume_desc",
-									"id_asc",
-									"id_desc",
-								],
-								error({errors}) {
-									TErrorFormatter(errors);
-								},
-							}),
-						),
-						category: t.Optional(
-							t.String({
-								examples: ["aave-tokens", "ai-applications", "binance-peg-tokens"],
-								error({errors}) {
-									TErrorFormatter(errors);
-								},
-							}),
-						),
-					}),
-					detail: swaggerDetail(
-						"Get the latest cryptocurrencies data",
-						200,
-						"CryptoCurrencies",
-						true,
-					),
-				},
-			)
-			.get(
-				"/:slug/market_chart/range",
-				async ({query, params}) =>
-					detailCryptoMarketChart({
-						query: queryParamsEncode(query),
-						params,
-					}),
-				{
-					query: t.Object({
-						vs_currency: t.Optional(
-							t.String({
-								examples: ["usd", "btc", "eth"],
-								default: "usd",
-								minLength: 3,
-								error({errors}) {
-									TErrorFormatter(errors);
-								},
-							}),
-						),
-						from: t.Optional(
-							t.Number({
-								examples: [1711929600, 1712275200],
-								default: from.getTime() / 1000,
-								error({errors}) {
-									TErrorFormatter(errors);
-								},
-							}),
-						),
-						to: t.Optional(
-							t.Number({
-								examples: [1711929600, 1712275200],
-								default: to.getTime() / 1000,
-								error({errors}) {
-									TErrorFormatter(errors);
-								},
-							}),
-						),
-						precision: t.Optional(
-							t.Number({
-								examples: [0, 1, 2],
-								default: 2,
-								minimum: 1,
-								error({errors}) {
-									TErrorFormatter(errors);
-								},
-							}),
-						),
-					}),
-					params: t.Object({
-						slug: t.String({
-							examples: ["bitcoin", "ethereum", "tether"],
-							minLength: 3,
-							error({errors}) {
-								TErrorFormatter(errors);
-							},
-						}),
-					}),
-					detail: swaggerDetail(
-						"Get the market chart data of a cryptocurrency",
-						200,
-						"CryptoCurrencyGraph",
-						true,
-					),
-				},
-			)
-			.get("/:slug", async ({params}) => detailCryptoDescriptionData({params}), {
-				params: t.Object({
-					slug: t.String({
-						examples: ["bitcoin", "ethereum", "tether"],
-						minLength: 3,
-						error({errors}) {
-							TErrorFormatter(errors);
-						},
-					}),
-				}),
-				detail: swaggerDetail(
-					"Get the details data of a cryptocurrency",
-					200,
-					"CryptoCurrency",
-					true,
-				),
-			})
-			.get("/search", async ({query}) => searchCrypto({query}), {
-				query: t.Object({
-					query: t.String({
-						examples: ["bit", "bitc", "bitcoin"],
-						minLength: 1,
-						error({errors}) {
-							TErrorFormatter(errors);
-						},
-					}),
-				}),
-				detail: swaggerDetail(
-					"Get the cryptocurrency list based on the search query",
-					200,
-					"SearchCrypto",
-					true,
-				),
-			}),
-	);
+    return app.use(
+        new Elysia({
+            name: "Coin Gecko Routes",
+            aot: false,
+            prefix: "/coins",
+            detail: {
+                tags: [ "CoinGecko" ],
+            },
+        })
+            .get(
+                "/markets",
+                async ({ query }) =>
+                    listCryptoCurrencies({
+                        query: queryParamsEncode(query),
+                    }),
+                {
+                    query: t.Object({
+                        vs_currency: t.Optional(
+                            t.String({
+                                examples: [ "usd", "btc", "eth" ],
+                                default: "usd",
+                                minLength: 3,
+                                error({ errors }) {
+                                    TErrorFormatter(errors);
+                                },
+                            }),
+                        ),
+                        per_page: t.Optional(
+                            t.Number({
+                                examples: [ 12, 20, 50, 100 ],
+                                default: 12,
+                                minimum: 1,
+                                error({ errors }) {
+                                    TErrorFormatter(errors);
+                                },
+                            }),
+                        ),
+                        page: t.Optional(
+                            t.Number({
+                                examples: [ 1, 2, 5, 10 ],
+                                default: 1,
+                                minimum: 1,
+                                error({ errors }) {
+                                    TErrorFormatter(errors);
+                                },
+                            }),
+                        ),
+                        precision: t.Optional(
+                            t.Number({
+                                examples: [ 0, 1, 2 ],
+                                default: 2,
+                                minimum: 1,
+                                error({ errors }) {
+                                    TErrorFormatter(errors);
+                                },
+                            }),
+                        ),
+                        price_change_percentage: t.Optional(
+                            t.String({
+                                examples: [ "1h", "24h", "7d", "14d", "30d", "200d", "1y" ],
+                                error({ errors }) {
+                                    TErrorFormatter(errors);
+                                },
+                            }),
+                        ),
+                        order: t.Optional(
+                            t.String({
+                                examples: [
+                                    "market_cap_desc",
+                                    "market_cap_asc",
+                                    "volume_asc",
+                                    "volume_desc",
+                                    "id_asc",
+                                    "id_desc",
+                                ],
+                                error({ errors }) {
+                                    TErrorFormatter(errors);
+                                },
+                            }),
+                        ),
+                        category: t.Optional(
+                            t.String({
+                                examples: [ "aave-tokens", "ai-applications", "binance-peg-tokens" ],
+                                error({ errors }) {
+                                    TErrorFormatter(errors);
+                                },
+                            }),
+                        ),
+                    }),
+                    detail: swaggerDetail(
+                        "Get the latest cryptocurrencies data",
+                        200,
+                        "CryptoCurrencies",
+                        true,
+                    ),
+                },
+            )
+            .get(
+                "/:slug/market_chart/range",
+                async ({ query, params }) =>
+                    detailCryptoMarketChart({
+                        query: queryParamsEncode(query),
+                        params,
+                    }),
+                {
+                    query: t.Object({
+                        vs_currency: t.Optional(
+                            t.String({
+                                examples: [ "usd", "btc", "eth" ],
+                                default: "usd",
+                                minLength: 3,
+                                error({ errors }) {
+                                    TErrorFormatter(errors);
+                                },
+                            }),
+                        ),
+                        from: t.Optional(
+                            t.Number({
+                                examples: [ 1711929600, 1712275200 ],
+                                default: from.getTime() / 1000,
+                                error({ errors }) {
+                                    TErrorFormatter(errors);
+                                },
+                            }),
+                        ),
+                        to: t.Optional(
+                            t.Number({
+                                examples: [ 1711929600, 1712275200 ],
+                                default: to.getTime() / 1000,
+                                error({ errors }) {
+                                    TErrorFormatter(errors);
+                                },
+                            }),
+                        ),
+                        precision: t.Optional(
+                            t.Number({
+                                examples: [ 0, 1, 2 ],
+                                default: 2,
+                                minimum: 1,
+                                error({ errors }) {
+                                    TErrorFormatter(errors);
+                                },
+                            }),
+                        ),
+                    }),
+                    params: t.Object({
+                        slug: t.String({
+                            examples: [ "bitcoin", "ethereum", "tether" ],
+                            minLength: 3,
+                            error({ errors }) {
+                                TErrorFormatter(errors);
+                            },
+                        }),
+                    }),
+                    detail: swaggerDetail(
+                        "Get the market chart data of a cryptocurrency",
+                        200,
+                        "CryptoCurrencyGraph",
+                        true,
+                    ),
+                },
+            )
+            .get("/:slug", async ({ params }) => detailCryptoDescriptionData({ params }), {
+                params: t.Object({
+                    slug: t.String({
+                        examples: [ "bitcoin", "ethereum", "tether" ],
+                        minLength: 3,
+                        error({ errors }) {
+                            TErrorFormatter(errors);
+                        },
+                    }),
+                }),
+                detail: swaggerDetail(
+                    "Get the details data of a cryptocurrency",
+                    200,
+                    "CryptoCurrency",
+                    true,
+                ),
+            })
+            .get("/search", async ({ query }) => searchCrypto({ query }), {
+                query: t.Object({
+                    query: t.String({
+                        examples: [ "bit", "bitc", "bitcoin" ],
+                        minLength: 1,
+                        error({ errors }) {
+                            TErrorFormatter(errors);
+                        },
+                    }),
+                }),
+                detail: swaggerDetail(
+                    "Get the cryptocurrency list based on the search query",
+                    200,
+                    "SearchCrypto",
+                    true,
+                ),
+            })
+            .get("/search/trending", async () => getTrendingCryptos(), {
+                detail: swaggerDetail(
+                    "Get the trending cryptocurrencies",
+                    200,
+                    "TrendingCryptos",
+                    true,
+                ),
+            }),
+    );
 }
