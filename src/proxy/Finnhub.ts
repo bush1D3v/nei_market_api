591de8062d2dae349f929cce7ec79abef98a4f3e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2025-02-11 18:51:05 -0300

    bomb

--- src/proxy/Finnhub.ts
+++ src/proxy/Finnhub.ts
@@ -1,17 +1,17 @@
-import {get} from "@/helpers/HttpClient";
-import type {New} from "@/types/Finnhub/New";
-import type {Category} from "@/types/Finnhub/Category";
-import type {ElysiaCustomStatusResponse} from "elysia/dist/error";
-import catchErrors, {type CatchError} from "@/errors/catcher";
+import { get } from "@/helpers/HttpClient";
+import type { New } from "@/types/Finnhub/New";
+import type { Category } from "@/types/Finnhub/Category";
+import type { ElysiaCustomStatusResponse } from "elysia/dist/error";
+import catchErrors, { type CustomError, type CatchError } from "@/errors/catcher";
 
 const BASE_API_URL = Bun.env.FINNHUB_HOST;
 
 const defaultHeaders = {
-	"X-Finnhub-Token": Bun.env.FINNHUB_KEY,
+    "X-Finnhub-Token": Bun.env.FINNHUB_KEY,
 };
 
 interface ListingsMarketNewsQueryParams {
-	category?: Category;
+    category?: Category;
 }
 
 /**
@@ -22,30 +22,35 @@ interface ListingsMarketNewsQueryParams {
  * @throws {ElysiaCustomStatusResponse<number, CatchError>} If the request to the external API fails
  */
 export async function listMarketNews({
-	query,
-}: {query: ListingsMarketNewsQueryParams}): Promise<
-	New[] | ElysiaCustomStatusResponse<number, CatchError>
+    query,
+}: { query: ListingsMarketNewsQueryParams }): Promise<
+    New[] | ElysiaCustomStatusResponse<number, CatchError>
 > {
-	const categoryQuery = `?category=${query.category}`;
-	const url = `${BASE_API_URL}/api/v1/news${categoryQuery}`;
+    const categoryQuery = `?category=${query.category}`;
+    const url = `${BASE_API_URL}/api/v1/news${categoryQuery}`;
 
-	try {
-		const response = await get(url, defaultHeaders);
+    try {
+        const response = await get(url, defaultHeaders);
 
-		if (!response.ok) throw new Error();
+        if (!response.ok) throw new Error();
 
-		const jsonData: New[] = await response.json();
+        const jsonData: New[] = await response.json();
 
-		return jsonData;
-	} catch (error: unknown) {
-		return catchErrors({status: 500, message: "Internal Server Error", error});
-	}
+        return jsonData;
+    } catch (error: unknown) {
+        const err = error as CustomError;
+        return catchErrors({
+            status: err?.status || 500,
+            message: err?.message || "Internal Server Error",
+            error,
+        });
+    }
 }
 
 interface ListingsCompanyNewsQueryParams {
-	symbol?: string;
-	from?: string;
-	to?: string;
+    symbol?: string;
+    from?: string;
+    to?: string;
 }
 
 /**
@@ -56,24 +61,29 @@ interface ListingsCompanyNewsQueryParams {
  * @throws {ElysiaCustomStatusResponse<number, CatchError>} If the request to the external API fails
  */
 export async function listCompanyNews({
-	query,
-}: {query: ListingsCompanyNewsQueryParams}): Promise<
-	New[] | ElysiaCustomStatusResponse<number, CatchError>
+    query,
+}: { query: ListingsCompanyNewsQueryParams }): Promise<
+    New[] | ElysiaCustomStatusResponse<number, CatchError>
 > {
-	const symbolQuery = `?symbol=${query.symbol}`;
-	const fromQuery = query.from ? `&from=${query.from}` : "";
-	const toQuery = query.to ? `&to=${query.to}` : "";
-	const url = `${BASE_API_URL}/api/v1/company-news${symbolQuery}${fromQuery}${toQuery}`;
+    const symbolQuery = `?symbol=${query.symbol}`;
+    const fromQuery = query.from ? `&from=${query.from}` : "";
+    const toQuery = query.to ? `&to=${query.to}` : "";
+    const url = `${BASE_API_URL}/api/v1/company-news${symbolQuery}${fromQuery}${toQuery}`;
 
-	try {
-		const response = await get(url, defaultHeaders);
+    try {
+        const response = await get(url, defaultHeaders);
 
-		if (!response.ok) throw new Error();
+        if (!response.ok) throw new Error();
 
-		const jsonData: New[] = await response.json();
+        const jsonData: New[] = await response.json();
 
-		return jsonData;
-	} catch (error: unknown) {
-		return catchErrors({status: 500, message: "Internal Server Error", error});
-	}
+        return jsonData;
+    } catch (error: unknown) {
+        const err = error as CustomError;
+        return catchErrors({
+            status: err?.status || 500,
+            message: err?.message || "Internal Server Error",
+            error,
+        });
+    }
 }
