fb5e06a04d4f8bc0c2ffaef60dc74d308e226305
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2025-02-14 13:18:44 -0300

    bomb

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import {Elysia} from "elysia";
-import {CoinGeckoRoutes} from "@/routes/CoinGecko";
-import {BrapiDevRoutes} from "@/routes/BrapiDev";
-import {FinnhubRoutes} from "@/routes/Finnhub";
-import {CurrencyQuotesRoutes} from "@/routes/CurrencyQuotes";
-import {GenerativeAIRoutes} from "@/routes/Gemini";
+import { Elysia } from "elysia";
+import { CoinGeckoRoutes } from "@/routes/CoinGecko";
+import { BrapiDevRoutes } from "@/routes/BrapiDev";
+import { FinnhubRoutes } from "@/routes/Finnhub";
+import { CurrencyQuotesRoutes } from "@/routes/CurrencyQuotes";
+import { GenerativeAIRoutes } from "@/routes/Gemini";
 import logger from "@/helpers/logger";
 import cors from "@/middlewares/cors";
 import rateLimiter from "@/middlewares/rateLimiter";
@@ -13,25 +13,25 @@ import handlePreflight from "@/middlewares/handlePreflight";
 import catchErrors from "@/errors/catcher";
 
 const app = new Elysia({
-	name: "NEI Market Analytics API",
-	aot: false,
-}).onRequest(({set, request}) => {
-	if (!request.url.endsWith("/swagger")) helmet(set);
+    name: "NEI Market Analytics API",
+    aot: false,
+}).onRequest(({ set, request }) => {
+    if (!request.url.endsWith("/swagger")) helmet(set);
 
-	if (request.method === "OPTIONS") handlePreflight(set);
+    if (request.method === "OPTIONS") handlePreflight(set);
 });
 
 const log = import.meta.env.SERVER_AMBIENT === "development" ? logger(app) : "";
 
 app.onError((ctx) => {
-	if (import.meta.env.SERVER_AMBIENT === "development" && log) log.error(ctx);
-	return catchErrors({
-		status: "status" in ctx.error ? ctx.error.status : 500,
-		message: ctx.error.message,
-		error: ctx.error.stack,
-	});
+    if (import.meta.env.SERVER_AMBIENT === "development" && log) log.error(ctx);
+    return catchErrors({
+        status: "status" in ctx.error ? ctx.error.status : 500,
+        message: ctx.error.message,
+        error: ctx.error.stack,
+    });
 });
-app.use(cors);
+//app.use(cors);
 app.use(rateLimiter);
 app.use(swagger);
 app.use(CoinGeckoRoutes);
