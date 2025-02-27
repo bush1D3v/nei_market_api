7581d2e0bceadc63bb16668115ee6195e6c6aad3
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2025-02-27 18:19:38 -0300

    bomb

--- src/server.ts
+++ src/server.ts
@@ -2,23 +2,23 @@ import app from "./app";
 import { Server } from "socket.io";
 import { setSocket } from "@/proxy/Gemini";
 import { corsOptions } from "./config/cors";
+import http from "node:http";
 
-const io = new Server({
-    cors: corsOptions
-}).listen(Bun.env.SOCKET_PORT, {
-    cors: corsOptions
-});
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Servidor Socket.IO rodando!");
+})
 
-io.use((socket, next) => {
-    socket.request.headers[ "Access-Control-Allow-Methods" ] = corsOptions.methods.join(",");
-    socket.request.headers[ "Access-Control-Allow-Headers" ] = corsOptions.allowedHeaders.join(",");
-    socket.request.headers[ "Access-Control-Allow-Credentials" ] = corsOptions.credentials.toString();
-    socket.request.headers[ "Access-Control-Max-Age" ] = corsOptions.maxAge.toString();
-    next();
+const io = new Server(server, {
+    cors: corsOptions
 });
 
 io.on("connection", (socketConnection) => {
     setSocket(socketConnection);
+});
+
+server.listen(3001, () => {
+    console.log("🌐 Servidor Socket.IO rodando na porta 3001");
 });
 
 app.listen(Bun.env.SERVER_PORT);
