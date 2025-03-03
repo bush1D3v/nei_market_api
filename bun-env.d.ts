8c79fd6b52c0b420ec9c577693643a7b06cb1d64
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2025-03-02 23:12:57 -0300

    bomb

--- bun-env.d.ts
+++ bun-env.d.ts
@@ -1,20 +1,25 @@
 declare module "bun" {
-	interface Env {
-		BRAPI_HOST: string;
-		BRAPI_KEY: string;
-		CURRENCYQUOTES_HOST: string;
-		FINNHUB_HOST: string;
-		FINNHUB_KEY: string;
-		COINGECKO_HOST: string;
-		COINGECKO_KEY: string;
-		GEMINI_KEY: string;
-		CLIENT_HOST: string;
-		CLIENT_PORT: number;
-		COOKIE_SECRET: string;
-		SERVER_PORT: number;
-		SOCKET_PORT: number;
-		SERVER_AMBIENT: string;
-		LOCAL_HOST: string;
-		LOCAL_PORT: number;
-	}
+    interface Env {
+        BRAPI_HOST: string;
+        BRAPI_KEY: string;
+        CURRENCYQUOTES_HOST: string;
+        FINNHUB_HOST: string;
+        FINNHUB_KEY: string;
+        COINGECKO_HOST: string;
+        COINGECKO_KEY: string;
+        GEMINI_KEY: string;
+        CLIENT_HOST: string;
+        CLIENT_PORT: number;
+        COOKIE_SECRET: string;
+        SERVER_PORT: number;
+        SOCKET_PORT: number;
+        SERVER_AMBIENT: string;
+        LOCAL_HOST: string;
+        LOCAL_PORT: number;
+        PROXY_PORT: number;
+        DATABASE_URL: string;
+        GITHUB_CLIENT_ID: string;
+        GITHUB_CLIENT_SECRET: string;
+        JWT_SECRET: string;
+    }
 }
